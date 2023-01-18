59b688d623aac6f8dec3d22eeb6bb539f93b4ea5
Sayantan <sayantan358@gmail.com>
2023-01-19 01:24:23 +0530

    Added JavaScript Practice files

---
+++ Other Practices/JavaScript Practice/arrayDestructuring.js
@@ -0,0 +1,19 @@
+let data = [2,4,6,5]
+
+// let nums = ['a','b','c','d']
+// nums = data
+// console.log(nums)
+
+// let [a,b,c,d] = data
+// let [a,b,d] = data
+// let [a,b,,d] = data
+
+// console.log(d)
+
+
+let str = "I am Sayantan Ghosh TigerDeV1L".split(' ');
+
+let [a,b,...e] = str;
+
+console.log(str);
+console.log(e);
---
+++ Other Practices/JavaScript Practice/swapValues.js
@@ -0,0 +1,19 @@
+let a = 5;
+let b = 6;      // IN THIS LINE ";" IS MUST..DON'T KNOW WHY
+
+[a,b] = [b,a];      //ARRAY DESTRUCTURING
+
+console.log(a,b);
+
+
+//JavaScript program to swap two variables
+
+//take input from the users
+// let a = 5;
+// let b = 6;
+
+// // //using destructuring assignment
+// [a, b] = [b, a];
+
+// console.log(`The value of a after swapping: ${a}`);
+// console.log(`The value of b after swapping: ${b}`);
