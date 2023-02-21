a3e7ac96c4bc1cb887e0bda504d6994520661dd9
Sayantan <sayantan358@gmail.com>
2023-02-21 11:38:25 +0530

    added weather app final

--- All Projects/10. Weather App (JavaScript)/script.js
+++ All Projects/10. Weather App (JavaScript)/script.js
@@ -11,10 +11,77 @@ const sunsetTime = document.querySelector(".sunset span");
 const humidity = document.querySelector(".humidity-data");
 const visibility = document.querySelector(".visibility-data");
 const feelsLike = document.querySelector(".feels-like-data");
-const forecastCard = document.querySelectorAll(".forecast-card");
+const forecastCardDays = document.querySelectorAll(".days");
+const forecastCardTemp = document.querySelectorAll(".forecast-temp-data");
+const forecastRainData = document.querySelectorAll(".forecast-rain-data");
+const forecastHumidityData = document.querySelectorAll(
+  ".forecast-humidity-data"
+);
+const forecastCardImage = document.querySelectorAll(".forecast-image-data");
+const forecastDate = document.querySelectorAll(".forecast-date");
+const forecastCondition = document.querySelectorAll(
+  ".forecast-weather-condition"
+);
+
+
+
+const onPageLoad = async () => {
+  const defaultApiCall = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=e9f03c0935864a1ba58105924231102&q=kolkata&days=7`
+  );
+
+  const defaultWeatherData = await defaultApiCall.json();
+
+  currTemp.innerText = defaultWeatherData.current.temp_c;
+  // weatherImage.src = defaultWeatherData.current.condition.icon;
+  weatherStatus.innerText = defaultWeatherData.current.condition.text;
+  cityName.innerText = defaultWeatherData.location.name;
+  countryName.innerText = defaultWeatherData.location.country;
+  uvIndex.innerText = defaultWeatherData.current.uv;
+  windSpeed.innerText = `${defaultWeatherData.current.wind_kph} km/h`;
+  sunriseTime.innerText =
+    defaultWeatherData.forecast.forecastday[0].astro.sunrise;
+  sunsetTime.innerText =
+    defaultWeatherData.forecast.forecastday[0].astro.sunset;
+  humidity.innerText = defaultWeatherData.current.humidity;
+  visibility.innerText = defaultWeatherData.current.vis_km;
+  feelsLike.innerText = defaultWeatherData.current.feelslike_c;
+
+  for (let i = 0; i <= 6; i++) {
+    const dateData = defaultWeatherData.forecast.forecastday[i].date;
+    const dateString = dateData.split("-").join(",");
+    const date = new Date(dateString);
+
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    forecastDate[i].innerText = defaultWeatherData.forecast.forecastday[i].date
+    forecastCardDays[i].innerText = daysOfWeek[date.getDay()];
+    forecastCondition[i].innerText =
+      defaultWeatherData.forecast.forecastday[i].day.condition.text;
+    forecastCardTemp[i].innerText =
+      defaultWeatherData.forecast.forecastday[i].day.avgtemp_c;
+    forecastRainData[i].innerText =
+      defaultWeatherData.forecast.forecastday[i].day.daily_chance_of_rain;
+    forecastHumidityData[i].innerText =
+      defaultWeatherData.forecast.forecastday[i].day.avghumidity;
+    forecastCardImage[i].src =
+      defaultWeatherData.forecast.forecastday[i].day.condition.icon;
+  }
+};
+
+onPageLoad();
+
+
 
 let data;
-
 const getData = async (event) => {
   event.preventDefault();
 
@@ -31,7 +98,6 @@ const getData = async (event) => {
 
   const weatherData = await fetchData.json();
   data = weatherData;
-  console.log(data);
 
   currTemp.innerText = data.current.temp_c;
   weatherImage.src = data.current.condition.icon;
@@ -46,36 +112,32 @@ const getData = async (event) => {
   visibility.innerText = data.current.vis_km;
   feelsLike.innerText = data.current.feelslike_c;
 
-  let daysOfWeek;
   for (let i = 0; i <= 6; i++) {
-    daysOfWeek;
     const dateData = data.forecast.forecastday[i].date;
     const dateString = dateData.split("-").join(",");
     const date = new Date(dateString);
-    switch (date.getDay()) {
-      case 0:
-        daysOfWeek = "Sunday";
-        break;
-      case 1:
-        daysOfWeek = "Monday";
-        break;
-      case 2:
-        daysOfWeek = "Tuesday";
-        break;
-      case 3:
-        daysOfWeek = "Wednesday";
-        break;
-      case 4:
-        daysOfWeek = "Thursday";
-        break;
-      case 5:
-        daysOfWeek = "Friday";
-        break;
-      case 6:
-        daysOfWeek = "Saturday";
-        break;
-      default:
-        break;
-    }
+
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    forecastDate[i].innerText = data.forecast.forecastday[i].date
+      .split("-")
+      .join("/");
+    forecastCardDays[i].innerText = daysOfWeek[date.getDay()];
+    forecastCondition[i].innerText =
+      data.forecast.forecastday[i].day.condition.text;
+    forecastCardTemp[i].innerText = data.forecast.forecastday[i].day.avgtemp_c;
+    forecastRainData[i].innerText =
+      data.forecast.forecastday[i].day.daily_chance_of_rain;
+    forecastHumidityData[i].innerText =
+      data.forecast.forecastday[i].day.avghumidity;
+    forecastCardImage[i].src = data.forecast.forecastday[i].day.condition.icon;
   }
 };
