c6c70a399257775f6c655a654e0ef7db247bb1f3
Sayantan <sayantan358@gmail.com>
2023-02-24 11:53:35 +0530

    added todo app incomplete

--- All Projects/11. Todo App React/todo-app/src/components/todo.js
+++ All Projects/11. Todo App React/todo-app/src/components/todo.js
@@ -2,16 +2,26 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import "../App.css";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
 function Todo() {
   return (
     <>
       <div className="wrapper-container">
         <div className="add-item">
-          <input type="text" placeholder="Add item..." />
-          <button className="plus-icon-container">
+          <input
+            type="text"
+            placeholder="Add item..."
+          />
+          <button className="plus-icon-container" title="Add Item">
             <FontAwesomeIcon className="plus-icon" icon={faPlus} />
           </button>
+        </div>
+        <div className="todo-list">
+          <div className="item">
+            <h3>Apple</h3>
+            <FontAwesomeIcon icon={faTrashCan} />
+          </div>
         </div>
       </div>
     </>
