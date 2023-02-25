60d93ab5d5d337d2c83afdabc00af318a0555cb4
Sayantan <sayantan358@gmail.com>
2023-02-25 12:01:47 +0530

    added todo app incomplete

--- All Projects/11. Todo App React/todo-app/src/components/todo.js
+++ All Projects/11. Todo App React/todo-app/src/components/todo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPen, faPlus } from "@fortawesome/free-solid-svg-icons";
 import "../App.css";
 import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 
@@ -9,18 +9,22 @@ function Todo() {
     <>
       <div className="wrapper-container">
         <div className="add-item">
-          <input
-            type="text"
-            placeholder="Add item..."
-          />
-          <button className="plus-icon-container" title="Add Item">
+          <input type="text" placeholder="Add item..." />
+          <button className="plus-icon-container" title="Add Todo">
             <FontAwesomeIcon className="plus-icon" icon={faPlus} />
           </button>
         </div>
         <div className="todo-list">
           <div className="item">
-            <h3>Apple</h3>
-            <FontAwesomeIcon icon={faTrashCan} />
+            <p>Apple</p>
+            <div className="list-icon-container">
+              <div className="pen-icon-container" title="Delete Todo">
+                <FontAwesomeIcon className="icon-pen" icon={faPen} />
+              </div>
+              <div className="trash-icon-container" title="Update Todo">
+                <FontAwesomeIcon className="icon-trash" icon={faTrashCan} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
