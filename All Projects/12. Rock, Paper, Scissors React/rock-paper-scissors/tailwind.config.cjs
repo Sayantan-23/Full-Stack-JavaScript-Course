5eb70ec63c2c04cc02fbcef44f858df9a0414491
Sayantan <sayantan358@gmail.com>
2023-03-01 12:17:02 +0530

    added rock paper scissors project incomplete

---
+++ All Projects/12. Rock, Paper, Scissors React/rock-paper-scissors/tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    },
+  },
+  plugins: [],
+}
