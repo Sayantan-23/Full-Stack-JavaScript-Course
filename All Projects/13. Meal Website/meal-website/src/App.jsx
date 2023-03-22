de885269a2ce34f1300789b9de22ac412879ae2e
Sayantan <sayantan358@gmail.com>
2023-03-22 11:50:01 +0530

    added meal website project

--- All Projects/13. Meal Website/meal-website/src/App.jsx
+++ All Projects/13. Meal Website/meal-website/src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import mainBg from "./assets/main-bg.jpg"
 
-function App() {
-  const [count, setCount] = useState(0)
+const API = "www.themealdb.com/api/json/v1/1/";
 
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{background:`linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.2)),url(${mainBg})`, backgroundSize:"cover"}} className="h-[100vh]">
+      <div className="navbar m-auto backdrop-blur-md justify-between fixed">
+        <div className="navbar m-auto justify-between max-w-7xl">
+        <div className="navbar-start w-auto">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 text-white"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-[#212529] text-white rounded-box w-52"
+            >
+              <li>
+                <a>Categories</a>
+              </li>
+              <li>
+                <a>Portfolio</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <Link to="/" className="normal-case text-xl text-[#f1faee]">
+            RecipeRealm
+          </Link>
+        </div>
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered rounded-[100vmax]"
+          />
+          </div>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- All Projects/13. Meal Website/meal-website/src/main.jsx
+++ All Projects/13. Meal Website/meal-website/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </React.StrictMode>,
 )
